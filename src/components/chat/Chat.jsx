5ba96b3f8423fb854a350d0f9574561b01a0ce74
naveenkumar_28 <naveenkumar.a.uplogic@gmail.com>
2025-09-18 10:42:09 +0530

    size is big

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,34 +5,42 @@ import { SildeBar } from "../Sildebar/SildeBar";
 import { Nav } from "../nav/Nav";
 
 const ChatApp = () => {
-  const [showSidebar, setShowSidebar] = useState(false);
-  const [showNavbar, setShowNavbar] = useState(false);
+
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-gray-100 p-3">
-  <div
-    className="w-full h-full max-w-[110rem] py-2 px-4 shadow-xl rounded-4xl bg-[#212023] 
+         <div
+        className="py-2 px-4 shadow-xl rounded-4xl bg-[#212023] 
     grid grid-cols-1 sm:grid-cols-[70px_1fr] lg:grid-cols-[70px_3fr_auto] 
     gap-2 overflow-hidden"
-  >
-    {/* Navbar */}
-    <Nav
-      showSidebar={showSidebar}
-      setShowNavbar={setShowNavbar}
-      showNavbar={showNavbar}
-    />
+      >
 
-    {/* Messages */}
-    <Message />
 
-    {/* Sidebar / Advert Section */}
-    <aside className="hidden lg:block">
-      <SildeBar />
-    </aside>
-  </div>
-</div>
+      
+      <Nav
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+
+      {/* Messages */}
+      <Message
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+
+      {/* Sidebar / Advert Section */}
+      <aside className="hidden lg:block">
+        <SildeBar />
+      </aside> 
+
+     </div>
+      
+      </div>
+    
 
   );
 };
 
 export default ChatApp;
+
